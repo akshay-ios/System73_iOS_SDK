35bf40b884fd899188b3e1326cfd525329b6a45f
Akshay Kumar <akshay.kumar@system73.com>
2025-12-09 15:36:52 +0530

    create tag

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// System73
 import PackageDescription
 
 let package = Package(
