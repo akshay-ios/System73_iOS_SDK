1074d10cd3bab2c762a8201fa1b1a87131236854
Akshay Kumar <akshay.kumar@system73.com>
2025-12-09 15:31:25 +0530

    url changed

--- Package.swift
+++ Package.swift
@@ -23,18 +23,19 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PolyNetSDK",
-            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-package-manager/5.2.0.1.06.05.2025/polynet-ios-swift-package-manager-5.2.0.1.06.05.2025.zip",
-            checksum:"37793a111f4fbb9a0957dd9939838aaae03b73e502b170a070c13e99420c0577"
+            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-package-manager/5.2.3.09.12.2025/polynet-ios-swift-package-manager-5.2.3.09.12.2025.zip",
+            checksum:"5edbb9eaea7e82a350579dce87f9d9895f9978dadbad4b91f8a0dd9dd3e57dbb"
         ),
         .binaryTarget(
             name: "Starscream",
             url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-starscream-xcframework/4.0.8/polynet-ios-starscream-xcframework-4.0.8.zip",
-            checksum: "6c4564721d8d75e47aadc99b96374c556a50dabe6dd95b21ab999913acd1c1eb"
+            checksum: "69c5e0240e84f362e2a126abc225201dd61ec3757e2bcfe135c1fbc578769416"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
             url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-protobuf-xcframework/1.26.0/polynet-ios-swift-protobuf-xcframework-1.26.0.zip",
-            checksum: "c95d9f14b4fa9a26f27b175741b6318b03f783cba12f422f1ddfee54954da94c"
+            checksum: "551d47ccc76531e39de41e1905503557d1cf2c715e4c5bde82cdc5e68f98e6f6"
         )
     ]
 )
+
---
+++ Sources/System73Wrapper/System73Wrapper.swift
@@ -0,0 +1,10 @@
+//
+//  Untitled.swift
+//  System73_iOS_SDK_XCFramework
+//
+//  Created by Akshay Kumar on 08/12/25.
+//
+
+public struct System73Wrapper {
+    public init() {}
+}
